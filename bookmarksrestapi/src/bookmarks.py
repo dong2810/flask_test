30362effe6798e00e62b333f14c7b6673cfe2313
HDV <nguyen.huy.dong@where.place>
2023-12-12 17:00:45 +0700

    add

--- bookmarksrestapi/src/bookmarks.py
+++ bookmarksrestapi/src/bookmarks.py
@@ -39,7 +39,10 @@ def handle_bookmarks():
             'updated_at': bookmark.updated_at,
         }), HTTP_201_CREATED
     else:
-        bookmarks = Bookmark.query.filter_by(user_id=current_user)
+        page = request.args.get('page', 1, type=int)
+        per_page = request.args.get('per_page', 5, type=int)
+
+        bookmarks = Bookmark.query.filter_by(user_id=current_user).paginate(page=page, per_page=per_page)
         data = []
 
         for bookmark in bookmarks:
@@ -53,4 +56,82 @@ def handle_bookmarks():
                 'updated_at': bookmark.updated_at,
             })
 
-        return jsonify({'data': data}), HTTP_200_OK
+            meta = {
+                "page": bookmarks.page,
+                'pages': bookmarks.pages,
+                'total_count': bookmarks.total,
+                'prev_page': bookmarks.prev_num,
+                'next_page': bookmarks.next_num,
+                'has_next': bookmarks.has_next,
+                'has_prev': bookmarks.has_prev
+
+            }
+
+        return jsonify({'data': data, "meta": meta}), HTTP_200_OK
+    
+@bookmarks.get("/<int:id>")
+@jwt_required()
+def get_bookmark(id):
+    current_user = get_jwt_identity()
+
+    bookmark = Bookmark.query.filter_by(user_id=current_user, id=id).first()
+
+    if not bookmark:
+        return jsonify({'message': 'Item not found'}), HTTP_404_NOT_FOUND
+
+    return jsonify({
+        'id': bookmark.id,
+        'url': bookmark.url,
+        'short_url': bookmark.short_url,
+        'visit': bookmark.visits,
+        'body': bookmark.body,
+        'created_at': bookmark.created_at,
+        'updated_at': bookmark.updated_at,
+    }), HTTP_200_OK    
+
+@bookmarks.delete("/<int:id>")
+@jwt_required()
+def delete_bookmark(id):
+    current_user = get_jwt_identity()
+
+    bookmark = Bookmark.query.filter_by(user_id=current_user, id=id).first()
+
+    if not bookmark:
+        return jsonify({'message': 'Item not found'}), HTTP_404_NOT_FOUND
+
+    db.session.delete(bookmark)
+    db.session.commit()
+
+    return jsonify({}), HTTP_204_NO_CONTENT 
+
+@bookmarks.put("/<int:id>")
+@bookmarks.patch("/<int:id>")
+@jwt_required()
+def edit_bookmark(id):
+    current_user = get_jwt_identity()
+    bookmark = Bookmark.query.filter_by(user_id = current_user, id = id).first()
+    if not bookmark:
+        return jsonify({'message': 'Item not found'}), HTTP_404_NOT_FOUND
+    
+    body = request.get_json().get('body', '')
+    url = request.get_json().get('url', '')
+
+    if not validators.url(url):
+        return jsonify({
+            'error': 'Enter a valid URL'
+        }), HTTP_400_BAD_REQUEST
+    
+    bookmark.url = url
+    bookmark.body = body
+
+    db.session.commit()
+
+    return jsonify({
+        'id': bookmark.id,
+        'url': bookmark.url,
+        'short_url': bookmark.short_url,
+        'visit': bookmark.visits,
+        'body': bookmark.body,
+        'created_at': bookmark.created_at,
+        'updated_at': bookmark.updated_at,
+    }), HTTP_200_OK
